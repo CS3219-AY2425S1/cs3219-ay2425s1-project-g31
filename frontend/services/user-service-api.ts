041aabaa8296c3f2b972a9a61cafc2e05c59a4f0
shishirbychapur <shishirbychapur@gmail.com>
2024-09-29 18:29:23 +0800

    feat: add /auth/reset route

--- frontend/services/user-service-api.ts
+++ frontend/services/user-service-api.ts
@@ -1,6 +1,7 @@
-import axiosInstance from './axios-middleware'
-import axios from 'axios'
+import { IEmailVerificationDto } from '@repo/user-types'
 import { IUserDto } from '@repo/user-types'
+import axios from 'axios'
+import axiosInstance from './axios-middleware'
 
 // POST /users
 export const signUpRequest = async (userData: IUserDto): Promise<IUserDto | undefined> => {
@@ -46,7 +47,27 @@ export const loginRequest = async (userData: IUserDto): Promise<IUserDto | undef
 
             switch (statusCode) {
                 case 404:
-                    throw new Error('No such user, please try again!')
+                    throw new Error('Account not found. Please sign up and try again.')
+            }
+        } else {
+            throw new Error('An unexpected error occurred')
+        }
+    }
+}
+
+// POST /auth/reset
+export const sendResetPasswordEmail = async (userData: IEmailVerificationDto): Promise<number | undefined> => {
+    try {
+        const response = await axiosInstance.post<IEmailVerificationDto>('/auth/reset', userData)
+        return response.status
+    } catch (error) {
+        if (axios.isAxiosError(error)) {
+            const statusCode = error.response?.status
+            switch (statusCode) {
+                case 404:
+                    throw new Error('Account not found. Please sign up and try again.')
+                case 400:
+                    return statusCode
             }
         } else {
             throw new Error('An unexpected error occurred')
