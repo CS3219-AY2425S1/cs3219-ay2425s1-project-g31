59d476427571d9cd8c29ebfea9dde8bb8f7c0cab
shishirbychapur <shishirbychapur@gmail.com>
2024-09-29 19:00:30 +0800

    feat: add new backend endpoint to support update of pw w/o authentication

--- backend/user-service/src/routes/auth.routes.ts
+++ backend/user-service/src/routes/auth.routes.ts
@@ -1,11 +1,13 @@
+import { handleLogin, handleReset, handleUpdate, handleVerify } from '../controllers/auth.controller'
+
 import { Router } from 'express'
 import passport from 'passport'
-import { handleLogin, handleReset, handleVerify } from '../controllers/auth.controller'
 
 const router = Router()
 
 router.post('/login', passport.authenticate('local', { session: false }), handleLogin)
 router.post('/reset', handleReset)
 router.post('/verify', handleVerify)
+router.post('/update', handleUpdate)
 
 export default router
