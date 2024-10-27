282248077e3a65b521f87f21ec59d4115a743c61
shishirbychapur <shishirbychapur@gmail.com>
2024-10-27 12:20:41 +0800

    fix: resolve ci

--- backend/matching-service/src/services/ws.service.ts
+++ backend/matching-service/src/services/ws.service.ts
@@ -10,7 +10,7 @@ export class WebSocketConnection {
     private clients: Map<string, WebSocket> = new Map()
 
     constructor() {
-        this.wss = new WebSocketServer({ port: process.env.WS_PORT })
+        this.wss = new WebSocketServer({ port: 3007 }) // glemen pr will change this
         this.wss.on('connection', (ws: WebSocket, req: IncomingMessage) => {
             const query = url.parse(req.url, true).query
             const websocketId = query.id as string
