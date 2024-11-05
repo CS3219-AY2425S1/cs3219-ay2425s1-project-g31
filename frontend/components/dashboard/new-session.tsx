b0f2afd411d0c2f244434f332404ec3d53ea3ed4
Low Han <lowhanlynn@Lows-MacBook-Air-2.local>
2024-11-06 00:49:02 +0800

    Handle no question

--- frontend/components/dashboard/new-session.tsx
+++ frontend/components/dashboard/new-session.tsx
@@ -85,6 +85,9 @@ export const NewSession = () => {
         const socket = new WebSocket(
             `${process.env.NEXT_PUBLIC_API_URL ?? 'ws://localhost:3006'}/matching/ws/?id=${websocketId}`
         )
+        setTimeout(() => {
+            socket.close()
+        }, 60000)
         socketRef.current = socket
         socketRef.current.onclose = () => {
             updateMatchmakingStatus(MatchingStatus.MATCH_NOT_FOUND)
