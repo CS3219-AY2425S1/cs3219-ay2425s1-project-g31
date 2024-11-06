0f56efabab633cbc946201e75c47a33bc74b2dcf
yunruu <yunruuchin@gmail.com>
2024-11-06 10:25:43 +0800

    feat: display topic and complexity of ongoing match if any

--- frontend/components/dashboard/resume-session.tsx
+++ frontend/components/dashboard/resume-session.tsx
@@ -1,10 +1,43 @@
+import { useRouter } from 'next/router'
 import { Button } from '../ui/button'
+import { IMatch } from '@repo/user-types'
+import { convertSortedComplexityToComplexity } from '@repo/question-types'
 
-export default function ResumeSession() {
+export default function ResumeSession({ match }: { match: IMatch }) {
+    const router = useRouter()
+    const handleResume = async () => {
+        router.push(`/code/${match.id}`)
+    }
+    const { category, complexity } = match
     return (
         <div className="border-solid border-2 border-gray-200 rounded flex flex-col w-dashboard p-6 min-h-[60vh] max-h-[90vh] overflow-auto justify-between">
-            You have an ongoing session.
-            <Button variant="primary" className="w-full mt-4">
+            <div className="space-y-6">
+                <h3 className="text-xl font-bold">Resume Session</h3>
+                <p>You have an ongoing session for:</p>
+                <form>
+                    <label htmlFor="topic" className="block font-bold mb-2">
+                        Topic
+                    </label>
+                    <input
+                        id="ongoing-topic"
+                        type="text"
+                        value={category}
+                        readOnly
+                        className="w-full p-2 text-sm border-solid border-2 border-gray-200 rounded bg-gray-50"
+                    />
+                    <label htmlFor="complexity" className="block font-bold mb-2 mt-5">
+                        Complexity
+                    </label>
+                    <input
+                        id="ongoing-complexity"
+                        type="text"
+                        value={convertSortedComplexityToComplexity(complexity)}
+                        readOnly
+                        className="w-full p-2 text-sm border-solid border-2 border-gray-200 rounded bg-gray-50"
+                    />
+                </form>
+            </div>
+            <Button variant="primary" className="w-full mt-4" onClick={handleResume}>
                 Resume
             </Button>
         </div>
