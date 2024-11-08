14d1de6e3ed59ef22d668cf9172c67c488be5a74
Davian Kho <daviankho1@gmail.com>
2024-11-09 03:00:33 +0800

    ci: fix chat merge conflict

--- frontend/pages/code/[id]/chat.tsx
+++ frontend/pages/code/[id]/chat.tsx
@@ -7,18 +7,6 @@ const formatTimestamp = (timestamp: string) => {
     return date.toLocaleTimeString([], { hour: '2-digit', minute: '2-digit', hour12: true }).toUpperCase()
 }
 
-const getChatBubbleFormat = (currUser: string, sessionUser: string | undefined, type: 'label' | 'text') => {
-    let format = currUser === sessionUser ? 'items-end ml-5' : 'items-start text-left mr-5'
-
-    if (type === 'text') {
-        format +=
-            currUser === sessionUser
-                ? ' bg-theme-600 rounded-xl text-white'
-                : ' bg-slate-100 rounded-xl p-2 text-slate-900'
-    }
-    return format
-}
-
 const Chat: FC<{ chatData: ChatModel[]; isViewOnly: boolean; handleSendMessage: (msg: string) => void }> = ({
     chatData,
     isViewOnly,
@@ -44,7 +32,6 @@ const Chat: FC<{ chatData: ChatModel[]; isViewOnly: boolean; handleSendMessage: 
         return format
     }
 
->>>>>>> 97d2738 (fix: solve chat issue)
     const handleKeyDown = (e: React.KeyboardEvent<HTMLInputElement>) => {
         if (e.key === 'Enter' && e.currentTarget.value.trim() !== '') {
             handleSendMessage(e.currentTarget.value)
@@ -62,11 +49,11 @@ const Chat: FC<{ chatData: ChatModel[]; isViewOnly: boolean; handleSendMessage: 
     return (
         <>
             <div className="overflow-y-auto p-3">
-                {chatData.length > 0 ? (
-                    chatData.map((chat, index) => (
+                {!!chatData?.length &&
+                    Object.values(chatData).map((chat, index) => (
                         <div
                             key={index}
-                            className={`flex flex-col gap-1 mb-5 ${getChatBubbleFormat(chat.senderId, session?.user.username, 'label')}`}
+                            className={`flex flex-col gap-1 mb-5 ${getChatBubbleFormat(chat.senderId, 'label')}`}
                         >
                             <div className="flex items-center gap-2">
                                 <h4 className="text-xs font-medium">{chat.senderId}</h4>
@@ -75,13 +62,13 @@ const Chat: FC<{ chatData: ChatModel[]; isViewOnly: boolean; handleSendMessage: 
                                 </span>
                             </div>
                             <div
-                                className={`text-sm py-2 px-3 text-balance break-words w-full ${getChatBubbleFormat(chat.senderId, session?.user.username, 'text')}`}
+                                className={`text-sm py-2 px-3 text-balance break-words w-full ${getChatBubbleFormat(chat.senderId, 'text')}`}
                             >
                                 {chat.message}
                             </div>
                         </div>
-                    ))
-                ) : (
+                    ))}
+                {(!chatData || !chatData?.length) && (
                     <p className="w-full text-center text-gray-400 text-sm my-1">No chat history</p>
                 )}
                 <div ref={chatEndRef}></div>
@@ -95,7 +82,6 @@ const Chat: FC<{ chatData: ChatModel[]; isViewOnly: boolean; handleSendMessage: 
                         onKeyDown={handleKeyDown}
                         value={value}
                         onChange={(e) => setValue(e.target.value)}
-                        aria-label="Send a message"
                         readOnly={isViewOnly}
                     />
                 </div>
