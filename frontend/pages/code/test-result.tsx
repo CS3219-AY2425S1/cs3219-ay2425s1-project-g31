0fe0feff0ab1e5b63c3717d23df355b0d78fdcb7
yunruu <yunruuchin@gmail.com>
2024-11-08 15:57:16 +0800

    fix: prevent undefined access

--- frontend/pages/code/test-result.tsx
+++ frontend/pages/code/test-result.tsx
@@ -2,17 +2,29 @@ import { IResponse } from '@repo/submission-types'
 
 export default function TestResult({ result, expectedOutput }: { result?: IResponse; expectedOutput: string }) {
     if (!result) return <div className="text-sm text-slate-400">No test results yet</div>
+
+    if (!result.status) {
+        return (
+            <div className="space-y-2">
+                <p className="text-lg font-semibold text-red-500">Unexpected server error</p>
+                <p className="text-sm">We are very sorry. Please try again later</p>
+            </div>
+        )
+    }
+
+    const { id, description } = result.status
+
     return (
         <div className="w-full">
             <div className="w-full">
-                {result.status?.id === 3 ? (
-                    <span className="text-lg font-semibold text-green-500">{result.status.description}</span>
+                {id === 3 ? (
+                    <span className="text-lg font-semibold text-green-500">{description}</span>
                 ) : (
-                    <span className="text-lg font-semibold text-red-500">{result.status.description}</span>
+                    <span className="text-lg font-semibold text-red-500">{description}</span>
                 )}
-                <p className="text-sm text-gray-600 mt-1">Runtime: {result.time} ms</p>
+                <p className="text-sm text-gray-600 mt-1">Runtime: {result.time || '-'} ms</p>
             </div>
-            {result.status.id === 6 && result.compile_output && (
+            {id === 6 && result.compile_output && (
                 <div className="mt-2 w-full">
                     <span className="text-sm font-semibold text-red-500">Error</span>
                     <p className="px-2 py-1 text-sm text-gray-600 mt-1 whitespace-pre-wrap rounded-lg bg-slate-100 w-full">
@@ -28,7 +40,7 @@ export default function TestResult({ result, expectedOutput }: { result?: IRespo
                     </p>
                 </div>
             )}
-            {(result.status.id === 3 || result.status.id == 4) && (
+            {(id === 3 || id == 4) && (
                 <div className="flex flex-col gap-2 text-sm">
                     <span className="mt-2">Your Output = </span>
                     <span className="px-2 py-1 rounded-lg bg-slate-100 whitespace-pre-wrap">
